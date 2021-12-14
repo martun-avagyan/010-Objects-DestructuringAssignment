f0c5295cc8c745f347676ec548032ce253d3bb51
Martun <65818128+martun-avagyan@users.noreply.github.com>
2021-12-14 16:56:34 +0400

    feat: 08 Task comments.

---
+++ 08Task.js
@@ -0,0 +1,5 @@
+"use strict";
+let person = { name: "Sarah", country: "Nigeria", job: "Developer" };
+// let name, country, job; (4 th line declares variables)
+const { name, country, job } = person;
+console.log(name);
