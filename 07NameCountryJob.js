9f10a0d7735a44da26074ef8842013a7548542f0
Martun <65818128+martun-avagyan@users.noreply.github.com>
2021-12-14 16:49:41 +0400

    feat: 07 Getting name country and job.

---
+++ 07NameCountryJob.js
@@ -0,0 +1,6 @@
+"use strict";
+let person = { name: "Sarah", country: "Armenia", job: "Developer" };
+const { name: name, country: country, job: job } = person;
+console.log(name); // "Sarah"
+console.log(country); // "Nigeria"
+console.log(job); // "Developer"
