853b0afedc9c6f6b0d3e411a1adf7694d5443d88
Martun <65818128+martun-avagyan@users.noreply.github.com>
2021-12-14 16:21:17 +0400

    feat: 04 Arguments in sum function.

---
+++ 04ArgumentsInSum.js
@@ -0,0 +1,9 @@
+"use strict";
+
+function sum(...args) {
+  return args.reduce((sum, current) => {
+    return sum + current;
+  });
+}
+console.log(sum(1, 2)); // prints 3
+console.log(sum(1, 2, 3)); // prints 6
