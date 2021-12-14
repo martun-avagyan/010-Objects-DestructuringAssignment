c94c8efad842919d93301043ad210096a803f2db
Martun <65818128+martun-avagyan@users.noreply.github.com>
2021-12-14 14:07:05 +0400

    feat: 02 Destructuring Name, Country and job

---
+++ 02NameCountryJobDestructuring.js
@@ -0,0 +1,8 @@
+"use strict";
+
+let person = { firstName: "Sarah", country: "Nigeria", job: "Developer" };
+
+const { firstName: firstName, country: country, job: job } = person;
+console.log(firstName);
+console.log(country);
+console.log(job);
